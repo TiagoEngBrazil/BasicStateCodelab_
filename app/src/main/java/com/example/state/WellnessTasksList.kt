fe416033539d05a3bd4b85b2dcf30e228819487d
TiagoEngBrazil <tiago.pimenta@ucsal.edu.br>
2022-10-06 17:26:22 -0300

    Initial commit

---
+++ app/src/main/java/com/example/state/WellnessTasksList.kt
@@ -0,0 +1,29 @@
+package com.example.state
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.state.ui.WellnessTaskItem
+
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit
+) {
+    LazyColumn(modifier = modifier) {
+        items(list) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) } )
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/state/WellnessViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.state
+
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+class WellnessViewModel : ViewModel() {
+    private val _task = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask>
+        get() = _task
+
+    fun remove(item: WellnessTask) {
+        _task.remove(item)
+    }
+    fun changeTaskChecked(item: WellnessTask, checked: Boolean) {
+        tasks.find { it.id == item.id }?.let { task ->
+            task.checked = checked
+        }
+    }
+}
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
